d25fed6ae119202cfbb7edc4e8f35ae3ac0f1dcd
nikhil47nick <nikhiljoji94@gmail.com>
2023-01-14 21:44:28 +0530

    Added:Usecase3

--- main/java/com/bridgelabz/Day21/MoodAnalyzer.java
+++ main/java/com/bridgelabz/Day21/MoodAnalyzer.java
@@ -2,18 +2,24 @@ package com.bridgelabz.Day21;
 
 public class MoodAnalyzer {
 
-    public String analyseMood(String message) throws MoodAnalyzerException{
+    public String analyseMood(String message) {
+
         try {
             if (message.contains("Happy")) {
                 return "Happy";
             } else if (message.contains("Sad")) {
                 return "Sad";
             }
-            return "";
-        }catch (NullPointerException e){
-            return "Happy";
+        }catch (MoodAnalyzerException e){
+            switch (e.getErrorMessage()){
+                case null:return e.getMessage();
+                break;
+                case "":return e.getMessage();
+                break;
+                default:break;
+            }
         }
-
+return null;
     }
 
 }
--- main/java/com/bridgelabz/Day21/MoodAnalyzerExceptionEnum.java
+++ main/java/com/bridgelabz/Day21/MoodAnalyzerExceptionEnum.java
@@ -1,14 +1,19 @@
 package com.bridgelabz.Day21;
 
 public enum MoodAnalyzerExceptionEnum  {
-    Null("Happy"),Empty("Empty");
+    Null("Happy",null),Empty("Empty","");
     private String name;
+    private String error;
 
-    MoodAnalyzerExceptionEnum(String name){
+    MoodAnalyzerExceptionEnum(String name,String error){
+        this.error = error;
         this.name = name;
     }
 
     public String getName(){
         return this.name;
+    }
+    public String getError(){
+        return this.error;
     }
 }
